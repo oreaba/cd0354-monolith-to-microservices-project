92f591ce1e7ab51e455dd9bdced6bd20ba9a283f
Mohammad H. Oreaba <m.h.oreaba@gmail.com>
2022-10-09 19:26:34 +0200

    changing the apiHost to the public address of the reverseproxy

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://ac259bd8a1b414be5be50c2b6f5ec1d3-364644503.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
